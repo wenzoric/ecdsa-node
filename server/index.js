8be1ae45928403f56fa7b4b545fd70f3a5db52b3
文茁 <wen.zhuo@belle.com.cn>
2023-07-11 15:06:23 +0800

    balance

--- server/index.js
+++ server/index.js
@@ -7,14 +7,16 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "03170c8e65ea4bd9ef2640b4ca9422a896b3cb8fc6504b8781b6184e93605a54d2": 100,
-  "03450455f29a719adf049aa0a932efae99f9fc54e1fb3535e44c517760e07046d8": 50,
-  "0257e7ffd01099ab016b7544bc2b5005ab70977be6e5301e4387f4abb54fad7409": 75,
+  "03170c8e65ea4bd9ef2640b4ca9422a896b3cb8fc6504b8781b6184e93605a54d2": 100, //c44d4b08ad5e6b92fb090256f2454c99ef1c8a7a0e7ab18da84b7e67a0af2476
+  "03450455f29a719adf049aa0a932efae99f9fc54e1fb3535e44c517760e07046d8": 50,  //313a34fcf6a94c8ef6f1058037dd82589d50d4a3c5550e8e1d2f6aa372eda1a7
+  "0257e7ffd01099ab016b7544bc2b5005ab70977be6e5301e4387f4abb54fad7409": 75,   //657be0ca96b0c7d7a4df8653e083c60cf7e4c674213c1a8db5334ac9efeede2c
 };
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
+  console.log('address:',address);
   const balance = balances[address] || 0;
+  console.log('balance:',balance);
   res.send({ balance });
 });
 
